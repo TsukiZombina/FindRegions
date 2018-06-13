c7ed0954687397d109d5b6eef58a47607facd00b
mariomm <mariomartinezmolina@live.com>
2018-06-12 22:55:21 -0500

    Read input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <set>
+#include <utility>
 
-int main(int argc, char const* argv[])
+using std::cout;
+using std::cin;
+using std::endl;
+using std::set;
+using std::pair;
+
+int main()
 {
-    
+    int n;
+    scanf("%d", &n);
+
+    set<pair<float, float>> lines;
+
+    for(unsigned int i = 0; i < n; i++)
+    {
+        pair<float, float> line;
+        cin >> line.first >> line.second;
+
+        lines.insert(line);
+    }
+
+    cout << n;
+
     return 0;
 }
